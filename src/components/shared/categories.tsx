ca0d73dc3c417eab1486f6cfa10716c9a1c6a871
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-09-19 09:06:34 +0200

    added product cards

--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -1,24 +1,38 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import React from 'react';
 
 interface Props {
     className?: string;
 }
 
-const cats = ['Pizzas', 'Combo', 'Snacks', 'Cocktails', 'Coffe', 'Drinks', 'Desserts']
-const activeIndex = 0;
+const cats = [
+    { id: 1, name: 'Pizzas' },
+    { id: 2, name: 'Combo' },
+    { id: 3, name: 'Snacks' },
+    { id: 4, name: 'Cocktails' },
+    { id: 5, name: 'Coffe' },
+    { id: 6, name: 'Drinks' },
+    { id: 7, name: 'Desserts' }
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+    const categoryActiveId = useCategoryStore((state) => state.activeId);
+
     return (
         <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
             {
-                cats.map((cat, index) => (
+                cats.map(({name, id}, index) => (
                     <a className={cn(
                         'flex items-center font-bold h-11 rounded-2xl px-5',
-                        activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary'
-                    )} key={index}>
+                        categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary'
+                    )} 
+                    href={`/#${name}`}
+                    key={index}>
                         <button>
-                            {cat}
+                            {name}
                         </button>
                     </a>
                 ))
