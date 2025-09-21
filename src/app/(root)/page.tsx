a740e6b89b277e993eec8b9d42004b71629c0bd1
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-09-21 09:07:02 +0200

    refactor

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,5 @@
-import { Container, Filters, Title, TopBar } from "@/components/shared"
-import { ProductsGroupList } from "@/components/shared/products-group-list"
+import { Container, Filters, Title, TopBar } from '@/shared/components/shared';
+import { ProductsGroupList } from '@/shared/components/shared/products-group-list';
 import { prisma } from '../../../prisma/prisma-client';
 
 export default async function Home() {
@@ -9,9 +9,9 @@ export default async function Home() {
         include: {
           ingredients: true,
           items: true,
-        }
+        },
       },
-    }
+    },
   });
 
   return (
@@ -20,19 +20,22 @@ export default async function Home() {
         <Title text="All pizzas" size="lg" className="font-extrabold" />
       </Container>
 
-      <TopBar categories={categories.filter((category) => category.products.length > 0)}/>
+      <TopBar
+        categories={categories.filter(
+          (category) => category.products.length > 0
+        )}
+      />
 
       <Container className="mt-10 pb-14">
         <div className="flex gap-[60px]">
-
           <div className="w-[250px]">
             <Filters />
           </div>
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              {
-                categories.map((category) => (
+              {categories.map(
+                (category) =>
                   category.products.length > 0 && (
                     <ProductsGroupList
                       key={category.id}
@@ -41,12 +44,11 @@ export default async function Home() {
                       items={category.products}
                     />
                   )
-                ))
-              }
+              )}
             </div>
           </div>
         </div>
       </Container>
     </>
-  )
+  );
 }
---
+++ src/shared/hooks/use-ingredients.ts
@@ -0,0 +1,28 @@
+import { Api } from "@/shared/services/api-client";
+import { Ingredient } from "@prisma/client";
+import React from "react";
+
+export const useIngredients = () => {
+    const [loading, setLoading] = React.useState(true)
+    const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+
+    React.useEffect(() => {
+        async function fetchIngredients() {
+            try {
+                setLoading(true);
+                const ingredients = await Api.ingredietns.getAll();
+                setIngredients(ingredients);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchIngredients();
+    }, []);
+
+     return { 
+        ingredients, 
+        loading,
+    };
+}
