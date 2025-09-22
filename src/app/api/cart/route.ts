2b919105dff94e8864c35d7e221cad9775c19735
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-09-22 12:40:38 +0200

    add product to cart

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../prisma/prisma-client';
+import crypto from 'crypto';
+import { findOrCreateCart } from '@/shared/lib';
+import { CreateCartItemValues } from '@/shared/services/dto/cart.dto';
+import { updateCartTotalAmount } from '@/shared/lib/update-cart-total-amount';
 
 export async function GET(req: NextRequest) {
   try {
@@ -32,7 +36,66 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json({ cart: userCart });
   } catch (error) {
-    console.log(error);
+    console.log('[CART_GET] Server error', error);
+    return NextResponse.json(
+      { message: 'Failed to get cart' },
+      { status: 500 }
+    );
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    let token = req.cookies.get('cartToken')?.value;
+
+    if (!token) {
+      token = crypto.randomUUID();
+    }
+
+    const userCart = await findOrCreateCart(token);
+
+    const data = (await req.json()) as CreateCartItemValues;
+
+    const findCartItem = await prisma.cartItem.findFirst({
+      where: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        ingredients: { every: { id: { in: data.ingredients } } },
+      },
+    });
+
+    // if item found
+    if (findCartItem) {
+      await prisma.cartItem.update({
+        where: {
+          id: findCartItem.id,
+        },
+        data: {
+          quantity: findCartItem.quantity + 1,
+        },
+      });
+    } else {
+      await prisma.cartItem.create({
+        data: {
+          cartId: userCart.id,
+          productItemId: data.productItemId,
+          quantity: 1,
+          ingredients: { connect: data.ingredients?.map((id) => ({ id })) },
+        },
+      });
+    }
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+    // TODO: add max age to token
+    const resp = NextResponse.json(updatedUserCart);
+    resp.cookies.set('cartToken', token);
+
+    return resp;
+  } catch (error) {
+    console.error('[CART_POST] Server error', error);
+    return NextResponse.json(
+      { message: 'Failed to create cart' },
+      { status: 500 }
+    );
+  }
+}
