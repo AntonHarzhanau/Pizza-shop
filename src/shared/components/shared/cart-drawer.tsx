380e47f19cac792650383adfcab24c0ebe41a82e
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-09-23 06:41:26 +0200

    Added display of empty cart

--- src/shared/components/shared/cart-drawer.tsx
+++ src/shared/components/shared/cart-drawer.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
@@ -11,12 +12,15 @@ import {
 } from '@/shared/components/ui/sheet';
 import Link from 'next/link';
 import { Button } from '../ui';
-import { ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { CartDrawerItem } from './cart-drawer-item';
 import { getCartItemDetails } from '@/shared/lib';
 import { useCartStore } from '@/shared/store';
 import { PizzaSize, PizzaType } from '@/shared/constants/pizza';
 import { useShallow } from 'zustand/react/shallow';
+import Image from 'next/image';
+import { Title } from '.';
+import { cn } from '@/shared/lib/utils';
 
 interface Props {
   className?: string;
@@ -26,17 +30,22 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
   children,
   className,
 }) => {
-  // const totalAmount = useCartStore((s) => s.totalAmount);
-  // const items = useCartStore((s) => s.items);
-  // const fetchCartItems = useCartStore((s) => s.fetchCartItems);
-  // const updateItemQuantity = useCartStore((s) => s.updateItemQuantity);
-  // const removeCartItem = useCartStore((s) => s.removeCartItem);
-
-  const [totalAmount, items, fetchCartItems, updateItemQuantity, removeCartItem] = useCartStore(
-    useShallow((s) => [s.totalAmount, s.items, s.fetchCartItems, s.updateItemQuantity, s.removeCartItem])
+  const [
+    totalAmount,
+    items,
+    fetchCartItems,
+    updateItemQuantity,
+    removeCartItem,
+  ] = useCartStore(
+    useShallow((s) => [
+      s.totalAmount,
+      s.items,
+      s.fetchCartItems,
+      s.updateItemQuantity,
+      s.removeCartItem,
+    ])
   );
 
- 
   useEffect(() => {
     fetchCartItems();
   }, [fetchCartItems]);
@@ -55,58 +64,103 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
       <Sheet>
         <SheetTrigger asChild>{children}</SheetTrigger>
         <SheetContent className="flex flex-col justify-between pb-0 bg-[#f4f1ee]">
-          <SheetHeader>
-            <SheetTitle>
-              In cart <span className="font-bold">{items.length} items</span>
-            </SheetTitle>
-          </SheetHeader>
+          <div
+            className={cn(
+              'flex flex-col h-full',
+              !totalAmount && 'justify-center'
+            )}
+          >
+            {totalAmount > 0 && (
+              <SheetHeader>
+                <SheetTitle>
+                  In cart{' '}
+                  <span className="font-bold">{items.length} items</span>
+                </SheetTitle>
+              </SheetHeader>
+            )}
 
-          <div className="mx-2 mt-5 overflow-auto flex-1">
-            {items.map((item) => (
-              <div key={item.id} className="mb-2">
-                <CartDrawerItem
-                  id={item.id}
-                  imageUrl={item.imageUrl}
-                  details={
-                    item.pizzaSize && item.pizzaType
-                      ? getCartItemDetails(
-                          item.ingredients,
-                          item.pizzaType as PizzaType,
-                          item.pizzaSize as PizzaSize
-                        )
-                      : ''
-                  }
-                  name={item.name}
-                  price={item.price}
-                  quantity={item.quantity}
-                  onClickCountButton={(type) =>
-                    onClickCountButton(item.id, item.quantity, type)
-                  }
-                  onClickRemove={() => removeCartItem(item.id)}
+            {!totalAmount && (
+              <div className="flex flex-col items-center justify-center w-72 mx-auto">
+                <SheetTitle hidden />
+                <Image
+                  src={'/empty_cart.webp'}
+                  alt={'Empty cart'}
+                  width={120}
+                  height={120}
                 />
+                <Title
+                  text={'Your cart is empty'}
+                  size="sm"
+                  className="text-center font-bold my-2"
+                />
+                <p className="text-center text-neutral-500 mb-5">
+                  Add at least one product to your cart to complete your order.
+                </p>
+                <SheetClose asChild>
+                  <Button className="w-56 h-12 text-base" size="lg">
+                    <ArrowLeft className="w-5 mr-2" />
+                    Go back
+                  </Button>
+                </SheetClose>
               </div>
-            ))}
+            )}
+
+            {totalAmount > 0 && (
+              <>
+                <div className="mx-2 mt-5 overflow-auto flex-1">
+                  {items.map((item) => (
+                    <div key={item.id} className="mb-2">
+                      <CartDrawerItem
+                        id={item.id}
+                        imageUrl={item.imageUrl}
+                        details={
+                          item.pizzaSize && item.pizzaType
+                            ? getCartItemDetails(
+                                item.ingredients,
+                                item.pizzaType as PizzaType,
+                                item.pizzaSize as PizzaSize
+                              )
+                            : ''
+                        }
+                        disabled={item.disabled}
+                        name={item.name}
+                        price={item.price}
+                        quantity={item.quantity}
+                        onClickCountButton={(type) =>
+                          onClickCountButton(item.id, item.quantity, type)
+                        }
+                        onClickRemove={() => removeCartItem(item.id)}
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <SheetFooter className="mx-2 bg-white p-8">
+                  <div className="w-full">
+                    <div className="flex mb-4">
+                      <span className="flex flex-1 text-neutral-500">
+                        Total
+                        <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                      </span>
+
+                      <span className="font-bold text-lg">{totalAmount} €</span>
+                    </div>
+
+                    <Button
+                      asChild
+                      type="button"
+                      className="w-full h-12 text-base"
+                    >
+                      <Link href="/cart">
+                        Place an order
+                        <ArrowRight className="w-5 ml-2" />
+                      </Link>
+                    </Button>
+                  </div>
+                </SheetFooter>
+              </>
+            )}
           </div>
-
-          <SheetFooter className="mx-2 bg-white p-8">
-            <div className="w-full">
-              <div className="flex mb-4">
-                <span className="flex flex-1 text-neutral-500">
-                  Total
-                  <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-                </span>
-
-                <span className="font-bold text-lg">{totalAmount} €</span>
-              </div>
-
-              <Link href="/cart">
-                <Button type="submit" className="w-full h-12 text-base">
-                  Place an order
-                  <ArrowRight className="w-5 ml-2" />
-                </Button>
-              </Link>
-            </div>
-          </SheetFooter>
         </SheetContent>
       </Sheet>
     </div>
