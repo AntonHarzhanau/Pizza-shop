50671a3fad99ecdd735ba51176324241cda3cd21
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-10-02 15:14:44 +0200

    separate prisma schemas

---
+++ src/shared/components/shared/profile/profile-form.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import {
+  formRegisterSchema,
+  TFormRegisterValues,
+} from '../modals/auth-modal/froms/schema';
+import { User } from '@prisma/client';
+import toast from 'react-hot-toast';
+import { signOut } from 'next-auth/react';
+import { Container } from '../container';
+import { Title } from '../title';
+import { FormInput } from '../form-component';
+import { Button } from '../../ui';
+import { updateUserInfo } from '@/app/actions';
+
+interface Props {
+  data: User;
+}
+
+export const ProfileForm: React.FC<Props> = ({ data }) => {
+  const form = useForm({
+    resolver: zodResolver(formRegisterSchema),
+    defaultValues: {
+      fullname: data.fullname,
+      email: data.email,
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmit = async (data: TFormRegisterValues) => {
+    try {
+      await updateUserInfo({
+        email: data.email,
+        fullname: data.fullname,
+        password: data.password,
+      });
+
+      toast.success('Data updated!', {
+        icon: '✅',
+      });
+    } catch (error) {
+      console.error(error);
+      return toast.error('Error updating data!', {
+        icon: '❌',
+      });
+    }
+  };
+
+  const onClickSignOut = () => {
+    signOut({
+      callbackUrl: '/',
+    });
+  };
+
+  return (
+    <Container className="my-10 flex flex-col items-center">
+      <Title text="Personal data" size="md" className="font-bold" />
+
+      <FormProvider {...form}>
+        <form
+          className="flex flex-col gap-5 w-96 mt-10"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormInput name="fullname" label="Full name" required />
+          <FormInput name="email" label="E-mail" required />
+
+          <FormInput type="password" name="password" label="New password" />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            label="Repeat password"
+          />
+
+          <Button
+            disabled={form.formState.isSubmitting}
+            className="text-base mt-10"
+            type="submit"
+          >
+            Save
+          </Button>
+
+          <Button
+            onClick={onClickSignOut}
+            variant="secondary"
+            disabled={form.formState.isSubmitting}
+            className="text-base"
+            type="button"
+          >
+            Sign Out
+          </Button>
+        </form>
+      </FormProvider>
+    </Container>
+  );
+};
